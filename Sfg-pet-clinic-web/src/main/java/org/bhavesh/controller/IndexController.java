d5a6c12de229205877bdd4b183c17f66654183ee
Bhavesh Kabra <kabrabhavesh12@gmail.com>
2021-08-03 10:30:33 +0530

    Controllers and HTML added

--- Sfg-pet-clinic-web/src/main/java/org/bhavesh/controller/IndexController.java
+++ Sfg-pet-clinic-web/src/main/java/org/bhavesh/controller/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
 	@RequestMapping({"/","","index","index.html"})
 	public String getIndex(Model model)
 	{
-		model.addAttribute("IndexH1","Index Page Thymeleaf");
+		//model.addAttribute("IndexH1","Index Page Thymeleaf");
 		return "index";
 	}
 }
---
+++ Sfg-pet-clinic-web/src/main/java/org/bhavesh/controller/OwnerController.java
@@ -0,0 +1,15 @@
+package org.bhavesh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("owners")
+public class OwnerController {
+
+	@RequestMapping({"","/index","/index.html"})
+	public String getOwnersIndex()
+	{
+		return "owners/index";
+	}
+}
