caa5f425910f30166a39b06a6ce095a51b466e92
Bhavesh Kabra <kabrabhavesh12@gmail.com>
2021-08-16 15:27:33 +0530

    Jupiter Junit5

--- Sfg-pet-clinic-web/src/test/java/org/bhavesh/SfgPetClinicApplicationTests.java
+++ Sfg-pet-clinic-web/src/test/java/org/bhavesh/SfgPetClinicApplicationTests.java
@@ -1,9 +1,12 @@
 package org.bhavesh;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(value = SpringExtension.class)
 class SfgPetClinicApplicationTests {
 
 	@Test
